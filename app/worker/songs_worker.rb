2632c47f25ec3156be05c1daf95a901b23bf07f3
charlie  lee <leetochia@gmail.com>
2019-04-12 23:56:35 +0000

    Automatically backed up by Learn

--- app/worker/songs_worker.rb
+++ app/worker/songs_worker.rb
@@ -1,10 +1,10 @@
-class SongsWorker 
+class SongsWorker
   require 'csv'
-  include Sidekiq::Worker 
-  
+  include Sidekiq::Worker
+
   def perform(songs_file)
-    CSV.foreach(songs_file, headers: true) do |lead|
+    CSV.foreach(songs_file, headers: true) do |song|
       Customer.create(title: song[0], artist_name: song[1])
-    end 
-  end 
-end 
+    end
+  end
+end
